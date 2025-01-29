0006a45eb1c31f21cf2be5d0dedbda39b985ac71
ra1nfall <georg555ru@gmail.com>
2025-01-29 11:56:50 +0300

    hw2 task6

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
-        dog = dog - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
-        var friend = 19;
-        System.out.println(friend);
-        friend = friend + 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        System.out.println(firstBoxer + secondBoxer);
+        System.out.println(secondBoxer - firstBoxer);
         }
     }
