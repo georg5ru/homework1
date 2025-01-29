c34fa3c4192509cf4999355ec83bd096bcff9440
ra1nfall <georg555ru@gmail.com>
2025-01-29 11:54:42 +0300

    hw2 task5

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,12 @@ public class Main {
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
