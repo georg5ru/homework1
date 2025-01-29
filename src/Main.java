49b437330e714f7ac57dab08bec0880bada56c12
ra1nfall <georg555ru@gmail.com>
2025-01-29 12:03:29 +0300

    hw2 task8

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        var firstBoxer = 78.2;
-        var secondBoxer = 82.7;
-        System.out.println(firstBoxer + secondBoxer);
-        System.out.println(secondBoxer - firstBoxer);
-        System.out.println(secondBoxer % firstBoxer);
-        }
+        var hoursSummary = 640;
+        var hoursPerHuman = 8;
+        var humanSummary = hoursSummary / hoursPerHuman;
+        System.out.println("Всего работников в компании — " + humanSummary + " человек");
+        humanSummary = humanSummary + 94;
+        hoursPerHuman = hoursSummary / humanSummary;
+        System.out.println("Если в компании работает " + humanSummary + " человек, то всего " + hoursPerHuman + " часов работы может быть поделено между сотрудниками");
     }
+}
