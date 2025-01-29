0e0edc39ec08a8a68de6223eabf9b7ac020f1de2
ra1nfall <georg555ru@gmail.com>
2025-01-29 11:51:01 +0300

    hw2 task2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
