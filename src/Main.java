796223a83577a4cc73dad0ebd840453770eec98f
ra1nfall <georg555ru@gmail.com>
2025-01-30 12:45:33 +0300

    hw2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     public static void main(String[] args) {
+        //task1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        //task2
+        dog = 8.0;
+        cat = 3.6;
+        paper = 763789;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        //task3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        //task4
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        //task5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        //task6
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        System.out.println(firstBoxer + secondBoxer);
+        System.out.println(secondBoxer - firstBoxer);
+        //task7
+        firstBoxer = 78.2;
+        secondBoxer = 82.7;
+        System.out.println(firstBoxer + secondBoxer);
+        System.out.println(secondBoxer - firstBoxer);
+        System.out.println(secondBoxer % firstBoxer);
+        //task8
         var hoursSummary = 640;
         var hoursPerHuman = 8;
         var humanSummary = hoursSummary / hoursPerHuman;
