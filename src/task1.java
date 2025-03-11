d5a1cf91fd3bc5fd7a8b0b73eef557f355e39d31
ra1nfall~ <georg555ru@gmail.com>
2025-03-11 11:01:05 +0300

    hw_cycle_lesson_two done 2.0

--- src/task1.java
+++ src/task1.java
@@ -15,9 +15,8 @@ public class task1 {
             counter += 1;
         }
         System.out.println();
-        for (int i = 1; i <= 10; i += 1) {
+        for (int i = 10; i >= 1; i -= 1) {
             System.out.print(i + " ");
-            i += 1;
         }
         System.out.println();
         //task3
