e4dc9863b2b9e5a6e4db1a3fce2b7ed1b8fd3ae4
ra1nfall~ <georg555ru@gmail.com>
2025-03-26 11:22:59 +0300

    hw_massive done 2.0

--- src/task1.java
+++ src/task1.java
@@ -3,25 +3,62 @@ import java.util.Arrays;
 public class task1 {
     public static void main(String[] args) {
         //task1
-        int[] moneySpend = {42, 52, 1488, 1000, 7};
-        int summa = 0;
-        for (int i = 0; i <= 4; i++) {
-            summa += moneySpend[i];
-        }
-        System.out.println("Сумма трат за месяц составила " + summa + " рублей");
+        int[] first = {1, 2, 3};
+        double[] second = {1.57, 7.654, 9.986};
+        int[] third = new int[5];
         //task2
-        int maximum = 0;
-        int minimum = 999999999;
-        for (int i = 0; i <= 4; i++) {
-            if (moneySpend[i] > maximum) {
-                maximum = moneySpend[i];
-            }
-            if (moneySpend[i] < minimum) {
-                minimum = moneySpend[i];
+        System.out.println("task2");
+        for (int i = 0; i < first.length; i++) {
+            System.out.print(first[i]);
+            if (i != first.length - 1) {
+                System.out.print(", ");
             }
         }
-        System.out.println("Минимальная сумма трат за неделю составила " + minimum + " рублей. Максимальная сумма трат за неделю составила " + maximum + " рублей");
+        System.out.println();
+        for (int i = 0; i < second.length; i++) {
+            System.out.print(second[i]);
+            if (i != second.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < third.length; i++) {
+            System.out.print(third[i]);
+            if (i != third.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
         //task3
-        System.out.println("Средняя сумма трат за месяц составила " + (summa / 5) + " рублей");
+        System.out.println("task3");
+        for (int i = 2; i >= 0; i--) {
+            System.out.print(first[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 2; i >= 0; i--) {
+            System.out.print(second[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 4; i >= 0; i--) {
+            System.out.print(third[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        //task4
+        System.out.println("task4");
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] % 2 != 0) {
+                first[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(first));
     }
 }
